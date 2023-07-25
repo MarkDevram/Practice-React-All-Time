1eae914ebb82ad37c200fc33ec023cedab220ad2
MarkDevram <138137277+MarkDevram@users.noreply.github.com>
2023-07-25 23:00:53 +0530

    Modified in Heading

--- src/Components/ForwardingRefs/FwdRefReciver.js
+++ src/Components/ForwardingRefs/FwdRefReciver.js
@@ -3,6 +3,7 @@ import React from "react"
 const FwdRefReciver = React.forwardRef((props, _) => {
   return (
     <div>
+      <h1>Lecture : ForwardingRefs</h1>
       <input ref={props.inputRef} />
     </div>
   )
